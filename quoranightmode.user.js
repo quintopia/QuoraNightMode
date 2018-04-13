8125083c26d07edb194efea118d502dd51f90f47
David Rutter <quintopia@gmail.com>
2018-04-12 23:38:31 -0400

    version number didn't update?

--- quoranightmode.user.js
+++ quoranightmode.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Quora Night Mode
 // @namespace    http://rutteric.com
-// @version      1.27
+// @version      1.28
 // @description  change quora colors at night
 // @author       David Rutter
 // @match        https://*.quora.com/*
